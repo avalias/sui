18be229d9e3fc4815dfb2bb5f11624632a8eed2c
William Robertson <williamrobertson@mystenlabs.com>
2023-03-14 09:03:42 -0400

    fix

--- apps/wallet/src/ui/app/hooks/useActiveAddress.ts
+++ apps/wallet/src/ui/app/hooks/useActiveAddress.ts
@@ -5,9 +5,5 @@ import { activeAddressSelector } from '../redux/slices/account';
 import useAppSelector from './useAppSelector';
 
 export function useActiveAddress() {
-<<<<<<< HEAD
     return useAppSelector(activeAddressSelector);
-=======
-    return useAppSelector(activeAddressSelector) ?? null;
->>>>>>> dc173718b (work)
 }
